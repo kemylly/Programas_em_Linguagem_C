3ac5aef2c0f202f1279bbfc57d7dab7b1fbed037
Kemylly Cavalcante <55566427+kemylly@users.noreply.github.com>
2020-09-02 17:11:53 -0300

    Rename Codigo_produto.cpp to Codigo_tipo_produto.cpp

--- Codigo_tipo_produto.cpp
+++ Codigo_tipo_produto.cpp
@@ -16,27 +16,27 @@ int main()
     printf("Digite o codigo do produto:\n"); 
     scanf("%d", &codigo);
     
-    /* 1 - Alimento n„o-perecÌvel
-	2, 3 ou 4 - Alimento perecÌvel
-	5 ou 6 - Vestu·rio
+    /* 1 - Alimento n√£o-perec√≠vel
+	2, 3 ou 4 - Alimento perec√≠vel
+	5 ou 6 - Vestu√°rio
 	7 - Higiene pessoal
-	8, 9, 10 - UtensÌlios domÈsticos
-	Qualquer outro cÛdigo Inv·lido */
+	8, 9, 10 - Utens√≠lios dom√©sticos
+	Qualquer outro c√≥digo Inv√°lido */
     
 	if(codigo == 1)
 	{
-		//1 - Alimento n„o-perecÌvel
-		printf("\nO produto %s pertence a classifica de Alimento n„o-perecÌvel \n", produto);
+		//1 - Alimento n√£o-perec√≠vel
+		printf("\nO produto %s pertence a classifica de Alimento n√£o-perec√≠vel \n", produto);
 	}
 	else if((codigo == 2) || (codigo == 3) || (codigo == 4))
 	{
-		//2, 3 ou 4 - Alimento perecÌvel
-		printf("\nO produto %s pertence a classifica de Alimento perecÌvel \n", produto);
+		//2, 3 ou 4 - Alimento perec√≠vel
+		printf("\nO produto %s pertence a classifica de Alimento perec√≠vel \n", produto);
 	}
 	else if((codigo == 5) || (codigo == 6))
 	{
-		//5 ou 6 - Vestu·rio
-		printf("\nO produto %s pertence a classifica de Vestu·rio \n", produto);
+		//5 ou 6 - Vestu√°rio
+		printf("\nO produto %s pertence a classifica de Vestu√°rio \n", produto);
 	}
 	else if(codigo == 7)
 	{
@@ -45,8 +45,8 @@ int main()
 	}
 	else if((codigo == 8) || (codigo == 9) || (codigo == 10))
 	{
-		//8, 9, 10 - UtensÌlios domÈsticos
-		printf("\nO produto %s pertence a classifica de UtensÌlios domÈsticos\n", produto);
+		//8, 9, 10 - Utens√≠lios dom√©sticos
+		printf("\nO produto %s pertence a classifica de Utens√≠lios dom√©sticos\n", produto);
 	}
 	else 
 	{
